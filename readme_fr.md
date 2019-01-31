216d06e6038c2288101cce477b69057c3c9f1cfe
loudemer <46995022+loudemer@users.noreply.github.com>
2019-01-31 08:28:02 +0100

    Add files via upload

--- readme_fr.md
+++ readme_fr.md
@@ -1,47 +1,67 @@
-Plugin Pluviometrie Z-Rain de Popp
+# Plugin Pluviometrie Z-Rain de Popp
+**Version 1.21**
+**Script cree par Germam**
 
 Lors de l'inclusion du materiel Z-Rain de Popp, la box Eedomus met en place 2 peripheriques l'un permettant de visualiser "le cumul de pluviometrie" en m3, l'autre permettant de voir la "pluviometrie horaire" en mm/h.
 
-Ce plugin permet de mettre en place un actionneur HTTP qui calcule la pluviometrie journaliere et mensuelle en mm a partir du cumul de pluviometrie et affiche les resultats dans 2 etats.
+Ce plugin permet de compléter cette installation en mettant en place un actionneur HTTP qui calcule la pluviometrie journaliere et mensuelle en mm a partir du cumul de pluviometrie et affiche les resultats dans 2 peripheriques.
 
 L'actionneur HTTP s'appuie sur un script PHP qui utilise l'API Eedomus.
 
-Pre-requis 
+## Pre-requis 
 
 Le pluviometre Z-Rain doit avoir ete installe avec presence des 2 peripheriques "pluviometrie horaire" et "cumul de pluviometrie".
 
-Installation du plugin
+## Installation du plugin
 
 Elle se fait a partir du store Eedomus en selectionnant le plugin "Pluviometrie ZRain".
 
-Lors de l'installation, il faut fournir l'ID du cumul de pluviometrie qui a ete installe lors de l'inclusion du module Z-Rain.
+### Champs a renseigner lors de l'installation du plugin
 
-Le plugin va installer 3 peripheriques : 
+- **designer la piece d'installation**
 
-1/ un actionneur HTTP : "controle de la pluviometrie" qui assure la mise a jour des pluviometries journalieres et mensuelles par le biais d'un script PHP et qui envoit les valeurs aux peripheriques respectifs.
+- **fournir l'ID du cumul de pluviometrie** qui a ete installe lors de l'inclusion du module Z-Rain.
 
-2/ un peripherique "cumul journalier" qui affiche la pluviometrie du jour
+Le plugin peut installer 5 peripheriques : 
 
-3/ un peripherique "cumul mensuel" qui affiche la pluviometrie du mois.
+        1/ un actionneur HTTP : "controle de la pluviometrie" qui assure la mise a jour des pluviometries journalieres et mensuelles par le biais d'un script PHP et qui envoit les valeurs aux peripheriques respectifs.
 
-Reglages complementaires
+        2/ un peripherique "cumul journalier" qui affiche la pluviometrie du jour
 
-1/ Il faut modifier les unites du controle "cumul de pluviometrie" mis en place lors de l'installation du pluviometre Z-Rain. Il faut passer les unites de m3 en mm. 
-Pour cela il faut aller dans le parametrage Z-Wave du pluviometre et modifier le parametre no 4 (Meter Multiplier) en lui assignant la valeur 1000.
+        3/ un peripherique "cumul mensuel" qui affiche la pluviometrie du mois.
 
-2/ Il est souhaitable mais pas indispensable de rajouter une regle de mise a jour journaliere des cumuls du type :
-Tous les jours a 0h et 1mn
-peripherique de pluviometrie MAJ.
-(cette regle n'a pu etre integree dans le plugin car le codage des declenchements horaires dans les regles n'est pas documente)
+        4/ un peripherique "Arrosage" qui affiche si un arrosage est necessaire en fonction du cumul de pluie des 3 derniers jours
 
-Fonctionnement
+        5/ un peripherique "Pluie Intense" qui affiche si la pluie depasse 5 mm / 5mn ou 17 mm/h
 
-Chaque bascule du pluviometre (tous les 0,5 mm de pluie) entraine une mise a jour automatique du cumul de pluie annuel.
-En effet, la modification du cumul annuel entraine le declenchement de la regle qui commande la mise a jour des cumuls journaliers et mensuels.
-Il n'est donc pas necessaire d'activer le polling de l'actionneur HTTP ce qui épargne du temps CPU.
+- installer ou non **les peripheriques "arrosage et Pluie Intense"** en cochant ou non la case.
 
-Extensions :
+## Reglages complementaires
 
-1/ Il est prevu de faire un releve de pluviometrie sur 3 jours pour pouvoir piloter un arrosage automatique fonction de la pluviometrie.
+1/ Il faut **modifier les unites du controle "cumul de pluviometrie"** mis en place lors de l'installation du pluviometre Z-Rain. Il faut passer les unites de m3 en mm. 
+Pour cela il faut aller dans le parametrage Z-Wave du pluviometre Z-Rain et modifier le parametre no 4 (Meter Multiplier) en lui assignant la valeur 1000.
 
-2/ Il serait aussi interessant d'affiner la notion de pluie forte (plus de 5 mm en 5 mn ou 17l/h). 
+
+## Fonctionnement
+
+Chaque bascule du pluviometre (tous les 0,5 mm de chute de pluie) entraine une mise a jour automatique du cumul de pluie annuel.
+
+La modification du cumul annuel entraine ensuite le declenchement d'une regle qui commande la mise a jour des cumuls journaliers et mensuels ainsi qu'une evaluation de l'intensite de la pluie et une evaluation du cumul de pluie pour conseiller ou non l'arrosage avec la mise à jour des controles respectifs.
+
+En cas de pluie intense (si le cumul de pluie depasse 5 mm/5mn ou 17 mm/h), l'peripherique "pluie intense" bascule à pluie intense ce qui vous permet d'ajouter une alerte avec  une regle basee sur la modification du peripherique pour la gerer.
+An noter que l'alerte se neutralise au bout de 5 mn à moins que l'episode de pluie intense continue.
+
+Pour l'arrosage, le peripherique "Arrosage" bascule a "conseille" lorsque le cumul de pluie des 3 derniers jours est inferieur à 5 mm et lorsqu'il ne pleut pas. A noter que l'utilisation du pluviometre est beaucoup plus precise que les releves internet surtout lors des orages qui peuvent etre tres localises.
+
+Il n'est pas necessaire d'activer le polling de l'actionneur HTTP ce qui epargne du temps CPU.
+
+Lorsque l'alerte pluie intense est déclenchee, la pluviometrie est reevalue automatiquement toutes les 5 mn pour desarmer, si besoin, l'alerte.
+
+La consigne d'arrosage (conseille ou non) est reevaluee chaque jour a minuit. On peut lancer une mise a jour dans une regle en lancant l'action "test arrosage" du peripherique "controle pluviometrie"
+
+## Nouveautes de la version 1.1
+
+Ajout des fonctionnalites **Pluie intense** et **Conseil arrosage**
+Corrections de bugs d'installation
+
+
--- zrain.php
+++ zrain.php
@@ -1,129 +1,251 @@
-<?php  
+<?  
     //**********************************************************************************************************
-    // V1.0 : Script de pluviometrie pour ZRain
+    // V1.21 : Script de pluviometrie pour ZRain
+    // Mise à jour : 31/01/2019
+    // Ajout des fonctions testarrosage et pluieintense
     // auteur: germam
     //*******************************************************************************************************
     // recuperation des infos depuis la requete
-    // API CONSO CUMULEE ANNEE ZRAIN - VAR1
+    // API DU PERIPHERIQUE CUMUL PLUIE ANNEE ZRAIN - [VAR1]
     $IDCumulZrain = getArg("id_cumul_zrain", $mandatory = true, $default = 'undefined');
-    // API CONSO CUMULEE MOIS - VAR2
-    $IDPluieMois = getArg("id_cumul_mois", $mandatory = true, $default = 'undefined');
-    // API DU PERIPHERIQUE APPELANT LE SCRIPT : cumul pluie jour
-    $IDpluieJour = getArg('id_cumul_jour', $mandatory = true, $default = 'undefined');
-    // ACTION
+    // API DES PERIPHERIQUE CUMULS PLUIE MOIS ET JOUR - [VAR2]
+    $IDListCumuls = getArg("id_cumuls", $mandatory = false, $default = 'undefined');
+    if ($IDListCumuls != 'undefined')
+    {
+        $Cumuls = explode(",",$IDListCumuls);
+        $IDPluieMois = $Cumuls[0];
+        $IDPluieJour = $Cumuls[1];
+    }
+    //API DES PERIPHERIQUES DE TEST D'ARROSAGE ET DE PLUIE INTENSE [VAR3]
+    $IDListTests = getArg("id_arrosage", $mandatory = false, $default = 'undefined');
+    if ($IDListTests != 'undefined')
+    {
+        $Tests = explode(",",$IDListTests);
+        $IDTestArrosage = $Tests[0];
+        $IDPluieIntense = $Tests[1];
+        $IDMacroPluieIntense = $Tests[2];
+    }
+    //ACTION DEMANDEE
     $action = getArg("action", $mandatory = true, $default = '');
+    // Nb de jours d'observation de pluviometrie pour declencher l'arrosage
+    $NbJourObservation = getArg("nbjoursobservation", $mandatory = false, $default = 3);
+    // Cumul minimum de pluie  en mm pour ne pas arroser
+    $CumulMini = getArg("cumulminisansarrosage", $mandatory = false, $default = 5);
+    // Seuil de declenchement de l'alerte pluie intense (normalement 5mm / 5mn ou 17mm / 1h)
+    $SeuilPluieIntense = getArg("seuilpluieintense", $mandatory = false, $default = 5);
+    $SeuilPluieIntenseMois = getArg("seuilpluieintensemois", $mandatory = false, $default = 17);
    
-    sdk_main($action,$IDCumulZrain,$IDPluieMois,$IDpluieJour);
+    sdk_main($action, $IDCumulZrain, $IDPluieMois, $IDPluieJour, $IDTestArrosage, $IDPluieIntense, $NbJourObservation, $CumulMini, $SeuilPluieIntense, $IDMacroPluieIntense, $SeuilPluieIntenseMois);
 
-    function sdk_main($action,$IDCumulZrain,$IDPluieMois,$IDpluieJour)
+    function sdk_main($action, $IDCumulZrain, $IDPluieMois, $IDPluieJour, $IDTestArrosage, $IDPluieIntense, $NbJourObservation, $CumulMini, $SeuilPluieIntense, $IDMacroPluieIntense, $SeuilPluieIntenseMois)
     {
         $zrain = new sdk_rain ();
-        $zrain->sdk_init($IDCumulZrain,$IDPluieMois,$IDpluieJour);
-
-        if($action == "updateconso"){
-            $zrain->sdk_updateconso();
-        } else if ($action == "raz") {
-            $zrain->sdk_raz();
+        $zrain->sdk_init($IDCumulZrain);
+        $peripharrosage = ($IDTestArrosage != "");
+        if ($IDCumulZrain != 'undefined')
+        {
+            switch ($action) 
+            {
+                case "updateconso" :
+                    $zrain->sdk_updateconso($IDPluieMois, $IDPluieJour );
+                    if ($peripharrosage)
+                    {
+                        $zrain->sdk_pluieintense($IDPluieIntense, $SeuilPluieIntense, $IDMacroPluieIntense, $SeuilPluieIntenseMois);
+                        $zrain->sdk_check_arrosage($IDTestArrosage, $NbJourObservation, $CumulMini);
+                    }
+                    break;
+                case "raz" : 
+                    $zrain->sdk_raz($IDPluieMois, $IDPluieJour);
+                    break;
+                case "arrosage" :
+                    if ($peripharrosage)
+                         $zrain->sdk_check_arrosage($IDTestArrosage, $NbJourObservation, $CumulMini);
+                    break;
+                case "pluieintense" :
+                    if ($peripharrosage)
+                        $zrain->sdk_pluieintense($IDPluieIntense, $SeuilPluieIntense);
+                    break;
+            }
         }
     }
   
     class sdk_rain 
     {
         protected $id_cumul_zrain;
-        protected $id_pluie_mois;
-        protected $id_pluie_jour;
-        protected $maintenant;
+        protected $formatdate;
+        protected $firstinstall;
          
-        public function sdk_init($IDCumulZrain , $IDPluieMois , $IDpluieJour) {
+        public function sdk_init($IDCumulZrain) 
+        {
             $this->id_cumul_zrain = $IDCumulZrain;
-            $this->id_pluie_mois = $IDPluieMois;
-            $this->id_pluie_jour = $IDpluieJour;
-            $this->maintenant = date("H").":".date("i");
+            $this->formatdate = "Y-m-d H:i:s";
+            $this->firstinstall = false;
         }
 
-        protected function sdk_init_releves_pluie() {
-            $tab_init = array ("jour_global" => 0.0000, "mois_global" => 0.0000, "last_cumul"=> 0.0000,
-            "last_date_mesure" => date('d')."-00:00");
+        protected function sdk_init_releves_pluie() 
+        {
+            $value = getValue($this->id_cumul_zrain);
+            $etat_compteur = $value['value']; 
+            
+            $tab_init = array ("jour_global" => 0.0000, "mois_global" => 0.0000, "last_cumul"=> 0.0000,"last_date_mesure" => date('d')."-00:00");
             saveVariable('RelevesPluie'.$this->id_cumul_zrain, $tab_init);
+            $this->firstinstall = true;
             return $tab_init;
         }
 
-        protected function sdk_get_last_releve_pluie() {
+        protected function sdk_get_last_releve_pluie() 
+        {
             $preload = loadVariable('RelevesPluie'. $this->id_cumul_zrain);
-            if ($preload != '' && substr($preload, 0, 8) != "## ERROR") {	
+            if ($preload != '' && substr($preload, 0, 8) != "## ERROR") 
+            {	
                 $tab_releve = $preload;
-            } else {
+            } else 
+            {
                 $tab_releve = $this->sdk_init_releves_pluie();
             }
             return $tab_releve;
         }
 
-        protected function sdk_save_new_releve_pluie($tab_releves) {
+        protected function sdk_save_new_releve_pluie($tab_releves) 
+        {
             $new_tab_releves = $tab_releves;
-            $new_tab_releves['last_date_mesure'] = date('d')."-".$this->maintenant;
+            $new_tab_releves['last_date_mesure'] = date('d')."-".date("H").":".date("i");
             saveVariable('RelevesPluie'.$this->id_cumul_zrain, $new_tab_releves);
         }
         
-        protected function sdk_update_control_value ( $releve_jour_global , $releve_mois_global){
-            setValue($this->id_pluie_jour,round($releve_jour_global,3));
-            setValue($this->id_pluie_mois,round($releve_mois_global,3));
+        protected function sdk_update_control_value ($IDPluieMois, $IDPluieJour, $releve_jour_global , $releve_mois_global)
+        {
+            if ($IDPluieJour != "")
+                setValue($IDPluieJour,round($releve_jour_global,3),false,true);
+            if ($IDPluieMois != "")
+                setValue($IDPluieMois,round($releve_mois_global,3),false,true);
         }
     
-        protected function sdk_save_last_releve_pluie($tab_cpt) {
-            saveVariable('CompteurPluie'.$this->id_cumul_zrain, $tab_cpt);
+        protected function sdk_get_cumul_periode ($delaijour, $delaiminute)
+        {
+            $StartDate = date($this->formatdate,mktime(date("H"),date("i") - $delaiminute, date("s"), date("m")  , date("d") - $delaijour , date("Y")));
+            $EndDate = date($this->formatdate);
+            
+            // recupere les enregistrements de cumul de pluie sur la periode 
+            $url = "https://api.eedomus.com/get?action=periph.history";
+            $url .= "&periph_id=" . $this->id_cumul_zrain;
+            $url .= "&start_date=" .$StartDate;
+            $url .= "&end_date=" .$EndDate;
+            $arr = sdk_json_decode(utf8_encode(httpQuery($url,'GET')));
+            $records = $arr["body"]["history"];
+            
+            // Calcul du cumul de pluie sur la pÃ©riode
+            $Cumul = 0;
+            $nbrecords=count($records);
+            if ($nbrecords > 1) 
+            {
+                $Cumul = (int)$records[0][0] - (int)$records[$nbrecords - 1][0];
+            }
+            else if ($nbrecords == 1)
+            {
+                $dernier_releve = $this->sdk_get_last_releve_pluie();
+                $last_etat_compteur = $dernier_releve["last_cumul"];
+                $Cumul = (int)$records[0][0] - $last_etat_compteur;
+            }
+            return $Cumul;
         }
         
-        public function sdk_updateconso() {
+        public function sdk_updateconso($IDPluieMois, $IDPluieJour) 
+        {
             // recuperation de la valeur actuelle du compteur
             $value = getValue($this->id_cumul_zrain);
             $etat_compteur = $value['value']; 
             
             // On recupere les dernieres mesures      
             $dernier_releve = $this->sdk_get_last_releve_pluie();
-            $last_etat_compteur = $dernier_releve["last_cumul"];
-            $releve_conso = 0;
-            
-            // recuperation du cumul depuis le dernier releve
-             if ($etat_compteur < $last_etat_compteur) {
-                $releve_conso = round($etat_compteur, 4);
-            }
-            else {
-                $releve_conso = round(($etat_compteur - $last_etat_compteur), 4);
-            }
-            
-            // recuperation des derniers releves sauvegardes
-            $lasttime = substr($dernier_releve['last_date_mesure'], 3, 5);
-            $lastday = substr($dernier_releve['last_date_mesure'], 0, 2);
-            $releve_jour_global = $dernier_releve['jour_global'];
-            $releve_mois_global = $dernier_releve['mois_global'];
-
-            // si la derniere mesure est la veille ou le mois precedent RAZ mesure
-            if ($lastday != date('d')) {
-                $releve_jour_global = 0;
-                if (date('j') == 1) {
-                    $releve_mois_global = 0;
-                }
-            }
+            if ($this->firstinstall == false)
+            {
+                $last_etat_compteur = $dernier_releve["last_cumul"];
+                $releve_conso = 0;
                 
-            // Mise a jour et sauvegarde des derniers releves
-            $releve_jour_global += $releve_conso;
-            $dernier_releve['jour_global'] = $releve_jour_global;
-            $releve_mois_global += $releve_conso;
-            $dernier_releve['mois_global'] = $releve_mois_global;
-            $dernier_releve["last_cumul"] = $etat_compteur;
-            $this->sdk_save_new_releve_pluie($dernier_releve);
-            
-            // Mise à jour des derniers releves sur les controles
-            $this->sdk_update_control_value ( $releve_jour_global , $releve_mois_global);
+                // recuperation du cumul depuis le dernier releve
+                if ($etat_compteur < $last_etat_compteur) 
+                {
+                    $releve_conso = round($etat_compteur, 4);
+                }
+                else 
+                {
+                    $releve_conso = round(($etat_compteur - $last_etat_compteur), 4);
+                }
+                
+                // recuperation des derniers releves sauvegardes
+                $lasttime = substr($dernier_releve['last_date_mesure'], 3, 5);
+                $lastday = substr($dernier_releve['last_date_mesure'], 0, 2);
+                $releve_jour_global = $dernier_releve['jour_global'];
+                $releve_mois_global = $dernier_releve['mois_global'];
+
+                // si la derniere mesure est la veille ou le mois precedent RAZ mesure
+                if ($lastday != date('d')) 
+                {
+                    $releve_jour_global = 0;
+                    if (date('j') == 1) 
+                    {
+                        $releve_mois_global = 0;
+                    }
+                }
+                    
+                // Mise a jour et sauvegarde des derniers releves
+                $releve_jour_global += $releve_conso;
+                $dernier_releve['jour_global'] = $releve_jour_global;
+                $releve_mois_global += $releve_conso;
+                $dernier_releve['mois_global'] = $releve_mois_global;
+                $dernier_releve["last_cumul"] = $etat_compteur;
+                $this->sdk_save_new_releve_pluie($dernier_releve);
+                // Mise a jour des derniers releves sur les controles
+                $this->sdk_update_control_value ($IDPluieMois, $IDPluieJour, $releve_jour_global , $releve_mois_global);
+            }
+            else
+                $this->sdk_update_control_value ($IDPluieMois, $IDPluieJour, 0 , 0);
         }
             
-        public function sdk_raz(){
+        public function sdk_raz($IDPluieMois, $IDPluieJour)
+        {
             $tab_init = $this->sdk_init_releves_pluie();
-            $this->sdk_update_control_value ( round($tab_init['jour_global'],3) , round($tab_init['mois_global'],3));
-            
-            $this->sdk_save_last_releve_pluie($tab_cpt);
+            $this->sdk_update_control_value ( $IDPluieMois, $IDPluieJour, 0 , 0);
+        }
+
+        public function sdk_check_arrosage($IDTestArrosage, $NbJourObservation, $CumulMini)
+        {
+            $cumul = $this->sdk_get_cumul_periode ($NbJourObservation, 0); 
+            $consigne = 1; /* inutile */
+            if ($cumul < $CumulMini)
+            { 
+                $consigne = 0; /*conseille*/
+                // On regarde s'il n'est pas en train de pleuvoir
+                $cumul = $this->sdk_get_cumul_periode (0, 5);
+                if ($cumul > 0)
+                    $consigne = 1;
+            }
+            setValue($IDTestArrosage, $consigne,false,true);
+        }
+
+        public function sdk_pluieintense($IDPluieIntense, $SeuilPluieIntense, $IDMacroPluieIntense, $SeuilPluieIntenseMois) 
+        {
+            if ($IDPluieIntense != "")
+            {
+                $consigne = 0; /* Pas de pluie intense */
+                // test si cumul sur 5 mn > $SeuilPluieIntense (5mm)
+                $cumul = $this->sdk_get_cumul_periode (0, 5); 
+                if ($cumul > $SeuilPluieIntense) 
+                    $consigne = 1; /* Pluie intense */
+                else
+                {           
+                    // test si cumul horaire > 17mm
+                    $cumul = $this->sdk_get_cumul_periode (1, 0); 
+                    if ($cumul > $SeuilPluieIntenseMois) 
+                        $consigne = 1;
+                }
+                if ($consigne == 1)
+                    setMacro($IDPluieIntense, $IDMacroPluieIntense);                
+                else
+                    setValue($IDPluieIntense,$consigne,false,true);
+            }
         }
     }
-        
     sdk_header('text/xml');
 ?>
